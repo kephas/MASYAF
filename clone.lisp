7c1918b02a7572eda76fce7cd3cc2f3c88439ae5
Pierre Thierry <pierre@nothos.net>
2011-07-17 05:37:45 +0200

    Added documetation and a list method in cloning.

--- clone.lisp
+++ clone.lisp
@@ -1,9 +1,13 @@
 (in-package :thierry-technologies.com/2010/01/masyaf)
 
+#| Generic functions for object cloning |#
+
 (defgeneric clone (object))
 
-(defgeneric shared-clone (object clone))
+(defgeneric shared-clone (object clone)) ; users are expected to define :after methods on it
 
+
+#| Base cases |#
 
 (defmethod clone (object)
   (let ((clone (make-instance (class-of object))))
@@ -11,3 +15,9 @@
     clone))
 
 (defmethod shared-clone (object clone))
+
+
+#| Shallow cloning for lists (for information bases) |#
+
+(defmethod clone ((object list))
+  (copy-list object))
