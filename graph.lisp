2878ee6fcc3c232ef00f8493b72b775787ddbba1
Pierre Thierry <pierre@nothos.net>
2011-07-17 05:56:24 +0200

    Minor modifications to cartesian points: Corrected NEXT-POINT and NEIGHBOURS to return CARTESIAN-POINT instances. Added PRINT-OBJECT method.

--- graph.lisp
+++ graph.lisp
@@ -4,6 +4,10 @@
   ((coordinates :reader point-coords :initarg :coords)
    (space-size :reader point-max :initarg :max :documentation "coordinates of the corner of the available space farthest from origin")))
 
+(defmethod print-object ((object cartesian-point) stream)
+  (print-unreadable-object (object stream :type t :identity t)
+    (format stream "~a <~a>" (point-coords object) (point-max object))))
+
 (defun origin-coordinates (dimensions)
   (if (zerop dimensions) nil (cons 0 (origin-coordinates (1- dimensions)))))
 
@@ -21,7 +25,10 @@
 			   (values (cons 0 following) t))
 		       (cons (1+ (first coordinates)) following))
 		   (cons (first coordinates) following)))))
-    (rec (point-coords point) (point-max point) t)))
+    (make-instance 'cartesian-point
+		   :coords (rec (point-coords point) (point-max point) t)
+		   :max (point-max point))))
+  
 
 (defun restack (from to)
   "Tail-recursive (append (reverse FROM) TO)"
@@ -42,4 +49,6 @@
 	       (if rest 
 		   (rec rest (rest maximums) (cons current previous) neighbours)
 		   neighbours))))
-    (rec (point-coords point) (point-max point) nil nil)))
+    (mapcar (lambda (coords)
+	      (make-instance 'cartesian-point :coords coords :max (point-max point)))
+	    (rec (point-coords point) (point-max point) nil nil))))
