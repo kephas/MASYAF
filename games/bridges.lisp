c96a77a2b9e36b9b2453be417ff075ea6f4ea273
Pierre Thierry <pierre@nothos.net>
2011-07-18 10:58:10 +0200

    Added bridge computation for the game of bridges.

---
+++ games/bridges.lisp
@@ -0,0 +1,47 @@
+(in-package :thierry-technologies.com/2010/01/masyaf)
+
+(defclass bridges-game (gamestate-with-information gamestate-with-space)())
+
+(defun except-first (fun &optional value)
+  (let ((first t))
+    (lambda (&rest args)
+      (if first
+	  (progn
+	    (setf first nil)
+	    value)
+	  (apply fun args)))))
+
+(defun turn (direction)
+  (case direction
+    ((horizontal) 'vertical)
+    ((vertical) 'horizontal)))
+
+(defun make-movement (direction orientation)
+  (make-translation (multiply (make-instance 'vector :space (make-instance 'cartesian-space :dim 2)
+					     :coords (case direction
+						       ((horizontal) '(1 0))
+						       ((vertical) '(0 1))))
+			      (if orientation 1 -1))))
+
+(defun make-path-to-island (game island direction orientation)
+  (make-path island (make-movement direction orientation)
+	     (except-first (lambda (point)
+			     (gamestate-info-search game (cons 'number (vect-coords point)))))))
+
+(defun possible-bridges (game island direction orientation)
+  (let ((wall (turn direction)))
+    (named-let rec ((path (make-path-to-island game island direction orientation))
+		    (possible 2))
+      (if path
+	  (cons-bind (first rest path)
+	    (if (zerop possible)
+		0
+		(rec rest (min possible
+			       (if (gamestate-info-search game `(bridge ,wall ,@(vect-coords first))) 0 2)
+			       (- 2 (length (gamestate-info-search game `(bridge ,direction ,@(vect-coords first)))))))))
+	  possible))))
+
+(defun add-bridge (game island direction orientation)
+  (map nil (except-first (lambda (point)
+			   (gamestate-add-info game `(bridge ,direction ,@(vect-coords point)))))
+       (make-path-to-island game island direction orientation)))
