0b47427a3cb5bf269a18510a71fff57415a15cb0
Pierre Thierry <pierre@nothos.net>
2011-07-19 04:51:06 +0200

    Added two brdiges agents and the char renderers.

--- games/bridges.lisp
+++ games/bridges.lisp
@@ -59,6 +59,10 @@
 	      (funcall fun game island direction orientation)))
 	  '((vertical . t)(vertical . nil)(horizontal . t)(horizontal . nil))))
 
+(defun possible-directions (game island)
+  (length (mapcan (lambda (n) (if (not (zerop n)) (list n)))
+		  (in-all-directions game island #'possible-bridges))))
+
 (defun sum (list)
   (reduce #'+ list))
 
@@ -66,6 +70,48 @@
   (- (island-number game island) (sum (in-all-directions game island #'existing-bridges))))
 
 (defun add-bridge (game island direction orientation)
-  (map nil (except-first (lambda (point)
-			   (gamestate-add-info game `(bridge ,direction ,@(vect-coords point)))))
-       (butlast (make-path-to-island game island direction orientation))))
+  (let* ((whole-path (make-path-to-island game island direction orientation))
+	 (endpoint (first (last whole-path))))
+    (gamestate-renew-info game (first (gamestate-info-search game `(number _ ,@(vect-coords endpoint)))))
+    (dolist (point (rest (butlast whole-path)) endpoint)
+      (gamestate-add-info game `(bridge ,direction ,@(vect-coords point))))))
+
+
+#| Agents |#
+
+(defvar *saturate*
+  (list '(number)
+	(lambda (game info)
+	  (let ((island (make-vector game (rest (information-arguments info)))))
+	    (if (= (needed-bridges game island)
+		   (sum (in-all-directions game island #'possible-bridges)))
+		(in-all-directions game island
+				   (lambda (game island dir orient)
+				     (dotimes (i (possible-bridges game island dir orient))
+				       (add-bridge game island dir orient))))))
+	  game)))
+
+(defvar *each-open-direction*
+  (list '(number)
+	(lambda (game info)
+	  (let ((island (make-vector game (rest (information-arguments info)))))
+	    (if (= (needed-bridges game island)
+		   (1- (* 2 (possible-directions game island))))
+		(in-all-directions game island
+				   (lambda (game island dir orient)
+				     (if (not (zerop (possible-bridges game island dir orient)))
+					 (add-bridge game island dir orient))))))
+	  game)))
+
+
+#| Renderers |#
+
+(defvar *bridges-renderers*
+  (list (lambda (game coords)
+	  (cif infos (gamestate-info-search game `(number _ ,@coords))
+	       (aref (format nil "~a" (first (information-arguments (first infos)))) 0)))
+	(lambda (game coords)
+	  (cif infos (gamestate-info-search game `(bridge _ ,@coords))
+	       (let ((chars '(horizontal "─═" vertical "│║")))
+		 (elt (getf chars (first (information-arguments (first infos)))) (1- (length infos))))))
+	(constantly #\space)))
