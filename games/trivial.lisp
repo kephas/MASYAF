0c7463227f425a71223514b32c1c3cbdf828da80
Pierre Thierry <pierre@nothos.net>
2011-07-22 13:05:57 +0200

    Corrected and moved back trivial game in main package.

--- games/trivial.lisp
+++ games/trivial.lisp
@@ -1,25 +1,18 @@
-(defpackage :thierry-technologies.com/2010/01/masyaf/trivial
-  (:use :cl :thierry-technologies.com/2010/01/masyaf)
-  (:export
-   #:test-trivial-game))
-   
-(in-package :thierry-technologies.com/2010/01/masyaf/trivial)
+(in-package :thierry-technologies.com/2010/01/masyaf)
 
 (defun make-alternating-agents (color1 color2)
   (labels ((make-agent (c1 c2)
 	     (list (list c1)
 		   (lambda (game info)
-		     (labels ((clause (color coords)
-				`(color ,color ,@coords)))
-		       (let ((point (make-instance 'sp-vector :coords (information-arguments info) :space (space-of game))))
-			 (dolist (coords (mapcar #'vect-coords (neighbours point 'manhattan-distance)) game)
-			   (unless (gamestate-info-search game `(color _ coords))
-			     (gamestate-add-info game (clause c2 coords))))))))))
+		     (let ((point (make-instance 'sp-vector :coords (rest (information-arguments info)) :space (space-of game))))
+		       (dolist (coords (mapcar #'vect-coords (remove-if (complement #'in-space?) (neighbours point 'manhattan-distance))) game)
+			 (unless (gamestate-info-search game `(color _ ,coords))
+			   (gamestate-add-info game `(color ,c2 ,coords)))))))))
     (list (make-agent color1 color2) (make-agent color2 color1))))
 
 (defclass trivial-game (gamestate-with-information gamestate-with-space)())
 
-(defvar *char-renderers*
+(defvar *trivial-game-char-renderers*
   (list (lambda (game coords)
 	  (cif infos (gamestate-info-search game `(color _ ,@coords))
 	       (if (> (length infos) 1)
@@ -32,4 +25,4 @@
   (let ((game (make-instance 'trivial-game :info nil :space (make-instance 'cartesian-hyperoctant :size (list width height)))))
     (gamestate-add-info game `(color black ,x ,y))
     (render-2d-spatial-game-by-char (solve game (make-alternating-agents 'black 'white))
-				    *char-renderers*)))
+				    *trivial-game-char-renderers*)))
